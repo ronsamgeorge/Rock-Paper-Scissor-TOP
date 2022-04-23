19582a99fc8caf8e99e71310b474171de1d26619
Ronald George <ronaldsamgeorge@gmail.com>
2022-04-23 17:18:06 +0530

    Add Main Game Logic

---
+++ script.js
@@ -0,0 +1,57 @@
+function computerPlay(){
+    let choices = ["Rock", "Paper", "Scissors "];
+    let randomChoiceNumber = Math.floor(Math.random()* 3);  //this will select a random number from 0-2
+    let selectedChoice = choices[randomChoiceNumber];
+    //console.log(randomChoiceNumber);
+    //console.log(selectedChoice);
+
+    return selectedChoice;
+}
+
+function playGame(playerSelection){
+
+    let playerChoice = playerSelection.toLowerCase();
+    let computerSelection = computerPlay().toLowerCase();
+
+    console.log("Player Choice : " + playerChoice);
+    console.log("PC : " + computerSelection);
+    
+    if (playerChoice == computerSelection){
+        return "Its a draw";
+    }
+
+    if (playerChoice == "scissors"){
+        if(computerSelection == "paper"){
+            return "You Win! Scissors beats paper"
+        }
+        else{
+            return "You Lose! Rock beats Scissors" 
+        }
+    }
+    
+    else if (playerChoice == "rock"){
+        if(computerSelection == "scissors"){
+            return "You Win! Rock beats Scissors";
+        }
+        else{
+            return "You Lose! Paper beats Rock";
+
+        }
+    }
+
+    else if (playerChoice == "paper"){
+        if (computerSelection == "rock"){
+            return "You win! Paper beats Rock!";
+        }
+        else {
+            return "You Lose! Scissors beats Rock"
+        }
+    }
+
+    else 
+    {
+        return "Wrong option";
+    }
+}
+
+console.log(playGame("paper"));
